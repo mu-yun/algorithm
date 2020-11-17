444a494a1eaee8b002da586de9bea9a7d20f666c
mu-yun <niu.muyun@gmail.com>
2020-11-17 21:34:14 +0800

    [21]LinkedList: merge two sorted lists

--- src/main/java/com/muyun/algorithm/collection/LinkedListOperation.java
+++ src/main/java/com/muyun/algorithm/collection/LinkedListOperation.java
@@ -35,10 +35,32 @@ public class LinkedListOperation {
         return true;
     }
 
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode sentry = new ListNode();
+        ListNode curr = sentry;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                curr.next = l2;
+                l2 = l2.next;
+            } else {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            curr = curr.next;
+
+        }
+        curr.next = l1 == null ? l2 : l1;
+
+        return sentry.next;
+    }
+
     class ListNode {
         int val;
         ListNode next;
 
+        ListNode() {
+        }
+        
         ListNode(int x) {
             val = x;
         }
