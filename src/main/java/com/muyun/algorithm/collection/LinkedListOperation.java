d35adc15cd81b724cc71f279d86d0697f0068add
mu-yun <niu.muyun@gmail.com>
2020-11-17 20:56:18 +0800

    [141]LinkedList: has cycle

--- src/main/java/com/muyun/algorithm/collection/LinkedListOperation.java
+++ src/main/java/com/muyun/algorithm/collection/LinkedListOperation.java
@@ -9,7 +9,7 @@ public class LinkedListOperation {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
-        while(curr!=null){
+        while (curr != null) {
             ListNode node = curr.next;
             curr.next = prev;
             prev = curr;
@@ -18,10 +18,30 @@ public class LinkedListOperation {
         return prev;
     }
 
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        //可以假设在head前有一个虚拟节点，slow和fast都前进了一步，不然循环条件无法直接正常运行
+        ListNode slowPoint = head;
+        ListNode fastPoint = head.next;
+        while (slowPoint != fastPoint) {
+            if (fastPoint == null || fastPoint.next == null) {
+                return false;
+            }
+            slowPoint = slowPoint.next;
+            fastPoint = fastPoint.next.next;
+        }
+        return true;
+    }
+
     class ListNode {
         int val;
         ListNode next;
-        ListNode(int x) { val = x; }
+
+        ListNode(int x) {
+            val = x;
+        }
     }
 
 }
